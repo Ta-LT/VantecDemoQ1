5bd9447ca1fc1c6fc468515152ebb530403f0e21
L T <5201249@qq.com>
2020-02-05 02:06:27 +0800

    added couselist

---
+++ VantecDemoQ1/courselist.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VantecDemoQ1
+{
+    public partial class courselist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            FpSpread1.ActiveSheetView.AllowSort = true;
+            FpSpread1.ActiveSheetView.AutoFilterMode = FarPoint.Web.Spread.AutoFilterMode.FilterBar;
+        }
+
+        protected void FpSpread1_InsertCommand(object sender, FarPoint.Web.Spread.SpreadCommandEventArgs e)
+        {
+            e.EditValues[12] = DateTime.Now;
+            e.EditValues[13] = DateTime.Now;
+        }
+
+        protected void FpSpread1_Ungrouped(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void FpSpread1_UpdateCommand(object sender, FarPoint.Web.Spread.SpreadCommandEventArgs e)
+        {
+
+        }
+
+        protected void FpSpread1_DeleteCommand(object sender, FarPoint.Web.Spread.SpreadCommandEventArgs e)
+        {
+
+        }
+
+        protected void FpSpread1_ErrorCommand(object sender, FarPoint.Web.Spread.SpreadCommandEventArgs e)
+        {
+
+        }
+    }
+}
